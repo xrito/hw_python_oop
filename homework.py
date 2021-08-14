eb84fc34e4be0c56c473e4bb5cf712082d0826ad
Дмитрий <xrito@yandex.ru>
2021-08-14 18:42:13 +0400

    4_commit

--- homework.py
+++ homework.py
@@ -20,22 +20,19 @@ class Calculator:
         self.limit = limit
         self.records = []
 
-    # добавляет новую запись в словарь
     def add_record(self, record):
+        # Добавляет новую запись в список.
         self.records.append(record)
 
-    # суммирует записи в списоке records
     def sum_add_records(self):
+        # Суммирует записи в списке records.
         return sum(record.amount for record in self.records)
-
-    # потрачено за день(без даты)
 
     def get_today_stats(self):
         now = dt.date.today()
         return sum(record.amount for record in self.records
                    if record.date == now)
 
-    # потрачено за неделю (с датой)
     def get_week_stats(self):
         now = dt.date.today()
         week_back = now - dt.timedelta(days=7)
@@ -50,27 +47,28 @@ class CashCalculator(Calculator):
     USD_RATE = 61.0
     EURO_RATE = 71.0
 
-    # остаток от лимита, в рублях или валюте
     def get_today_cash_remained(self, currency):
-        cur_l = {"eur": [self.EURO_RATE, 'Euro'], "usd": [
-            self.USD_RATE, 'USD'], "rub": [1, 'руб']}
-        cur_r = abs(self.remainder() / cur_l[currency][0])
-        if self.remainder() < 0:
+        remainder = self.remainder()
+        cur_l = {
+            'eur': [self.EURO_RATE, 'Euro'],
+            'usd': [self.USD_RATE, 'USD'],
+            'rub': [1, 'руб']
+        }
+        cur_r = abs(round((remainder / cur_l[currency][0]), 2))
+        currency_name = cur_l[currency][1]
+        if remainder < 0:
             return (f'Денег нет, держись: '
-                    f'твой долг - {cur_r:.2f} {cur_l[currency][1]}')
-        elif self.remainder() == 0:
-            return 'Денег нет, держись'
-        else:
-            return f'На сегодня осталось {cur_r:.2f} {cur_l[currency][1]}'
+                    f'твой долг - {cur_r} {currency_name}')
+        if remainder > 0:
+            return f'На сегодня осталось {cur_r} {currency_name}'
+        return 'Денег нет, держись'
 
 
 class CaloriesCalculator(Calculator):
 
-    # съедено за неделю (с датой)
     def get_calories_remained(self):
-        if self.remainder() >= 0:
-            return (f'Сегодня можно съесть что-нибудь ещё, '
-                    f'но с общей калорийностью не более '
+        if self.remainder() > 0:
+            return ('Сегодня можно съесть что-нибудь ещё, '
+                    'но с общей калорийностью не более '
                     f'{self.remainder()} кКал')
-        else:
-            return 'Хватит есть!'
+        return 'Хватит есть!'
